2413ea55e31a282225267ad5fd7bc14e394504f7
Heimir Þór Kjartansson <heimirkj@novomaticls.com>
2016-11-12 11:41:28 +0000

    moves url construction to file and tests
    tests generating url with postal code filters
    prepares tests for all documented filters in ABI
    modifies call in index.js to getting page count to expect a number

---
+++ test/construct-url.js
@@ -0,0 +1,56 @@
+const test = require('ava');
+const constructUrl = require('../lib/construct-url');
+
+test('constructing with a single postal code', (t) => {
+  const url = constructUrl({ 'postal-codes': [123] });
+
+  t.is(url, 'http://fasteignir.visir.is/ajaxsearch/getresults?zip=123&stype=sale');
+});
+
+test('constructing with multiple postal codes', (t) => {
+  const url = constructUrl({ 'postal-codes': [123, 456] });
+
+  t.is(url, 'http://fasteignir.visir.is/ajaxsearch/getresults?zip=123,456&stype=sale');
+});
+
+test.skip('constructing with a single street name', (t) => {
+});
+
+test.skip('constructing with multiple street names', (t) => {
+});
+
+test.skip('constructing with a single minimum price', (t) => {
+});
+
+test.skip('constructing with multiple minimum prices', (t) => {
+});
+
+test.skip('constructing with a single maximum price', (t) => {
+});
+
+test.skip('constructing with multiple maximum prices', (t) => {
+});
+
+test.skip('constructing with a single minimum square meters', (t) => {
+});
+
+test.skip('constructing with multiple minimum square meters', (t) => {
+});
+
+test.skip('constructing with a single maximum square meters', (t) => {
+});
+
+test.skip('constructing with multiple maximum square meters', (t) => {
+});
+
+test.skip('constructing with a single minimum number of rooms', (t) => {
+});
+
+test.skip('constructing with multiple minimum number of rooms', (t) => {
+});
+
+test.skip('constructing with a single maximum number of rooms', (t) => {
+});
+
+test.skip('constructing with multiple maximum number of rooms', (t) => {
+});
