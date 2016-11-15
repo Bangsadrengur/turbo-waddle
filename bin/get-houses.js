89d68fa966ac53b83c6a2cf59d47c9e4486c1437
Heimir Þór Kjartansson <heimirkj@novomaticls.com>
2016-11-15 23:22:33 +0000

    corrects cli -> fetch -> output process

--- bin/get-houses.js
+++ bin/get-houses.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 const execute = require('../lib/execute');
 
+const args = process.argv;
+
 /* eslint-disable no-console */
-execute()
+execute(args)
   .then(console.log)
   .catch(console.log);
